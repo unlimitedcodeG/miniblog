d0812b40ba73e0f2df04435b3a91e02c7dad8364
unlimitedcodeG <1659159807@qq.com>
2025-02-09 23:54:17 +0800

    server build2

--- websocketSer.py
+++ websocketSer.py
@@ -3,3 +3,17 @@ import asyncio
 import websocket
 
 import logging 
+
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def heartbeat(websocket):
+    " ping & pong"
+    while True:
+        try:
+            logging.info("Sending ping")
+            # 发送ping，并等待pong（pong_waiter 为Future对象）
+            pong_waiter = await websocket.ping()
+            await 
