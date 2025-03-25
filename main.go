f57e2399a496778c6e2c0c6791649160be21ac49
unlimitedcodeG <1659159807@qq.com>
2025-03-25 08:38:27 +0800

    codeing

--- main.go
+++ main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func f(from string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(from, ":", i)
-	}
+// 这是一个在 goroutine 中运行的函数。
+// `done` 通道用于通知其他 goroutine，该函数的任务已完成。
+func worker(done chan bool) {
+	fmt.Print("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	// 发送一个值，通知主 goroutine 任务完成
+	done <- true
 }
+
 func main() {
-	message := make(chan string)
+	// 创建一个带缓冲的通道 (缓冲大小为 1)
+	done := make(chan bool, 1)
 
-	go func() { message <- "ping" }()
+	// 启动 worker goroutine，并传递 `done` 通道
+	go worker(done)
 
-	msg := <-message
-	fmt.Println(msg)
+	// 阻塞，直到从 `done` 通道收到通知
+	<-done
 }
