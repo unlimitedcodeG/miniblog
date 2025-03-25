1e3ed6a48310071ff1bec44e9a074aad701a87e8
unlimitedcodeG <1659159807@qq.com>
2025-03-26 03:05:40 +0800

    codeing

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ func main() {
 	queue <- "two"
 
 	close(queue)
-
+	// ✅ 即使通道关闭，仍然可以继续接收已发送但尚未读取的数据。
+	// ✅ 使用 range 遍历通道时，通道关闭后会自动停止迭代，不会导致错误或死锁。
+	// ✅ 只有通道为空且已关闭时，range 才会结束遍历，否则会一直阻塞等待数据。
 	for elem := range queue {
 		fmt.Println(elem)
 	}
