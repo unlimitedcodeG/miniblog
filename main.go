db30ef620ab6ffa1393c6bb1a7ea9b3bb934fbdd
unlimitedcodeG <1659159807@qq.com>
2025-03-26 00:23:44 +0800

    channel

--- main.go
+++ main.go
@@ -1,28 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-// 这是一个在 goroutine 中运行的函数。
-// `done` 通道用于通知其他 goroutine，该函数的任务已完成。
-func worker(done chan bool) {
-	fmt.Print("working...")
-	time.Sleep(time.Second)
-	fmt.Println("done")
-
-	// 发送一个值，通知主 goroutine 任务完成
-	done <- true
-}
+import "fmt"
 
 func main() {
-	// 创建一个带缓冲的通道 (缓冲大小为 1)
-	done := make(chan bool, 1)
+	messages := make(chan string)
+	signals := make(chan bool)
 
-	// 启动 worker goroutine，并传递 `done` 通道
-	go worker(done)
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
+	}
 
-	// 阻塞，直到从 `done` 通道收到通知
-	<-done
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	default:
+		fmt.Println("no message sent")
+	}
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
 }
