8c2a40ac842b9c389f93319697b1542cd5418c49
unlimitedcodeG <1659159807@qq.com>
2025-03-27 23:10:32 +0800

    codeing

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	panic("a problem")
+	f := createFile("/tmp/defer.txt")
+	defer closeFile(f)
+	writeFile(f)
+}
 
-	_, err := os.Create("../file.txt")
+func createFile(p string) *os.File {
+	fmt.Println("creating")
+	f, err := os.Create(p)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func writeFile(f *os.File) {
+	fmt.Println("writing")
+	fmt.Fprintln(f, "data")
+}
+
+func closeFile(f *os.File) {
+	fmt.Println("closing")
+	err := f.Close()
+
 	if err != nil {
 		panic(err)
 	}
