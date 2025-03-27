445537ee1834cc0bfe3b5f10d69142170be9708e
unlimitedcodeG <1659159807@qq.com>
2025-03-27 22:07:56 +0800

    codeing

--- main.go
+++ main.go
@@ -1,42 +1,76 @@
 package main
 
 import (
-    "fmt"
-    "sync"
+	"fmt"
+	"math/rand"
+	"sync/atomic"
+	"time"
 )
 
-type Container struct {
-    mu       sync.Mutex
-    counters map[string]int
+type readOp struct {
+	key  int
+	resp chan int
 }
-
-func (c *Container) inc(name string) {
-
-    c.mu.Lock()
-    defer c.mu.Unlock()
-    c.counters[name]++
+type writeOp struct {
+	key  int
+	val  int
+	resp chan bool
 }
 
 func main() {
-    c := Container{
 
-        counters: map[string]int{"a": 0, "b": 0},
-    }
+	var readOps uint64
+	var writeOps uint64
 
-    var wg sync.WaitGroup
+	reads := make(chan readOp)
+	writes := make(chan writeOp)
 
-    doIncrement := func(name string, n int) {
-        for i := 0; i < n; i++ {
-            c.inc(name)
-        }
-        wg.Done()
-    }
+	go func() {
+		var state = make(map[int]int)
+		for {
+			select {
+			case read := <-reads:
+				read.resp <- state[read.key]
+			case write := <-writes:
+				state[write.key] = write.val
+				write.resp <- true
+			}
+		}
+	}()
 
-    wg.Add(3)
-    go doIncrement("a", 10000)
-    go doIncrement("a", 10000)
-    go doIncrement("b", 10000)
+	for r := 0; r < 100; r++ {
+		go func() {
+			for {
+				read := readOp{
+					key:  rand.Intn(5),
+					resp: make(chan int)}
+				reads <- read
+				<-read.resp
+				atomic.AddUint64(&readOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
 
-    wg.Wait()
-    fmt.Println(c.counters)
+	for w := 0; w < 10; w++ {
+		go func() {
+			for {
+				write := writeOp{
+					key:  rand.Intn(5),
+					val:  rand.Intn(100),
+					resp: make(chan bool)}
+				writes <- write
+				<-write.resp
+				atomic.AddUint64(&writeOps, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+
+	readOpsFinal := atomic.LoadUint64(&readOps)
+	fmt.Println("readOps:", readOpsFinal)
+	writeOpsFinal := atomic.LoadUint64(&writeOps)
+	fmt.Println("writeOps:", writeOpsFinal)
 }
