206505254107188670a51d029c5951a64638dec0
unlimitedcodeG <1659159807@qq.com>
2025-03-27 22:12:17 +0800

    codeing

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"slices"
 )
 
 func main() {
+	fruits := []string{"peach", "banana", "kiwi"}
 
-	strs := []string{"c", "a", "b"}
-	slices.Sort(strs)
-	fmt.Println("Strings:", strs)
+	lenCmp := func(a, b string) int {
+		return cmp.Compare(len(a), len(b))
+	}
 
-	ints := []int{7, 2, 4}
-	slices.Sort(ints)
-	fmt.Println("Ints:   ", ints)
+	slices.SortFunc(fruits, lenCmp)
+	fmt.Println(fruits)
 
-	s := slices.IsSorted(ints)
-	fmt.Println("Sorted: ", s)
+	type Person struct {
+		name string
+		age  int
+	}
+
+	people := []Person{
+		Person{name: "Jax", age: 37},
+		Person{name: "TJ", age: 25},
+		Person{name: "Alex", age: 72},
+	}
+
+	slices.SortFunc(people,
+		func(a, b Person) int {
+			return cmp.Compare(a.age, b.age)
+		})
+	fmt.Println(people)
 }
