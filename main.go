978f4586dfc115038a4e717ee8935949ec25fe0b
unlimitedcodeG <1659159807@qq.com>
2025-03-27 23:10:01 +0800

    codeing

--- main.go
+++ main.go
@@ -1,35 +1,13 @@
 package main
 
-import (
-	"cmp"
-	"fmt"
-	"slices"
-)
+import "os"
 
 func main() {
-	fruits := []string{"peach", "banana", "kiwi"}
 
-	lenCmp := func(a, b string) int {
-		return cmp.Compare(len(a), len(b))
+	panic("a problem")
+
+	_, err := os.Create("../file.txt")
+	if err != nil {
+		panic(err)
 	}
-
-	slices.SortFunc(fruits, lenCmp)
-	fmt.Println(fruits)
-
-	type Person struct {
-		name string
-		age  int
-	}
-
-	people := []Person{
-		Person{name: "Jax", age: 37},
-		Person{name: "TJ", age: 25},
-		Person{name: "Alex", age: 72},
-	}
-
-	slices.SortFunc(people,
-		func(a, b Person) int {
-			return cmp.Compare(a.age, b.age)
-		})
-	fmt.Println(people)
 }
