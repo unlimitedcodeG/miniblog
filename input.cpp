a73ac10a779512ebe66019aab7a6eedc53e6fec4
unlimitedcodeG <1659159807@qq.com>
2025-03-31 21:01:28 +0800

    test:cpp

--- input.cpp
+++ input.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 using namespace std;
 
+struct Student {
+    char name[10];  // 名字数组长度为10
+    int chinese;
+    int math;
+    int total;
+};
 
-int main(){
-   char ch;
-   cin >> ch;
+int main() {
+    Student a[3];  // 创建一个Student数组，大小为3
+    for (int j = 0; j < 3; j++) {
+        cin >> a[j].name >> a[j].math >> a[j].chinese;
+        a[j].total = a[j].math + a[j].chinese;  // 使用加法计算总分
+    }
 
-   ch = ((ch>='A')&&(ch<='Z'))?(ch -'A'+ 'a') :ch ;
-   cout <<ch; 
-   return 0;
+    for (int j = 0; j < 3; j++) {
+        cout << a[j].name << " " << a[j].chinese << " " << a[j].math << " "<< a[j].total << endl;
+    }
+    
+    return 0;
 }
--- main.go
+++ main.go
@@ -6,8 +6,23 @@ func mayPanic() {
 	panic("a problem")
 }
 
-func main() {
 
+
+func SliceDemo(){
+	var s1 [] = int 
+	s2 := make([]int,9)
+	fmt.Println(s2) 
+	s1 = make([]int,9,9)
+	print(s1)
+	s1[0]  = 0 
+	s1[1] = 1 
+	fmt.Println(s1[0:])	
+	s1 = append(s1, 1)
+	println("---",s1[0])
+
+}
+func main() {
+	SliceDemo()
 	defer func() {
 		if r := recover(); r != nil {
 
