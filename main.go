7e4dbff96608592066f47c242febf1926e0c5997
unlimitedcodeG <1659159807@qq.com>
2025-05-02 08:04:52 +0800

    codeing

--- main.go
+++ main.go
@@ -1,40 +1,19 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-	"sync"
 )
 
-type Container struct {
-	mu       sync.Mutex
-	counters map[string]int
-}
-
-func (c *Container) inc(name string) {
-	c.mu.Lock()         // 上锁：防止其他协程同时操作
-	defer c.mu.Unlock() // 函数退出前解锁（确保不会忘记）
-	c.counters[name]++  // 增加该 key 的计数
-}
-
 func main() {
-	c := Container{
-		counters: map[string]int{"a": 0, "b": 0},
-	}
 
-	var wg sync.WaitGroup
+	s := "sha this string"
+	h := sha256.New()
 
-	doIncrement := func(name string, n int) {
-		for i := 0; i < n; i++ {
-			c.inc(name)
-		}
-		wg.Done()
-	}
-	wg.Add(3)
-	go doIncrement("a", 10000)
-	go doIncrement("a", 10000)
-	go doIncrement("b", 10000)
+	h.Write([]byte(s))
 
-	wg.Wait()
-	fmt.Println(c.counters)
+	bs := h.Sum(nil)
 
+	fmt.Println(s)
+	fmt.Printf("%x\n", bs)
 }
