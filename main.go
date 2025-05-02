7c48287351b8106169af84c5fc12b06e98d4b786
unlimitedcodeG <1659159807@qq.com>
2025-05-02 09:24:30 +0800

    codeing

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"os"
+	"path/filepath"
 )
 
 func check(e error) {
@@ -15,46 +14,23 @@ func check(e error) {
 
 func main() {
 
-	dat, err := os.ReadFile("/tmp/dat")
-	check(err)
-	fmt.Print(string(dat))
-
-	f, err := os.Open("/tmp/dat")
+	f, err := os.CreateTemp("", "sample")
 	check(err)
 
-	b1 := make([]byte, 5)
-	n1, err := f.Read(b1)
-	check(err)
-	fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
+	fmt.Println("Temp file name:", f.Name())
 
-	o2, err := f.Seek(6, io.SeekStart)
-	check(err)
-	b2 := make([]byte, 2)
-	n2, err := f.Read(b2)
-	check(err)
-	fmt.Printf("%d bytes @ %d: ", n2, o2)
-	fmt.Printf("%v\n", string(b2[:n2]))
+	defer os.Remove(f.Name())
 
-	_, err = f.Seek(2, io.SeekCurrent)
+	_, err = f.Write([]byte{1, 2, 3, 4})
 	check(err)
 
-	_, err = f.Seek(-4, io.SeekEnd)
+	dname, err := os.MkdirTemp("", "sampledir")
 	check(err)
+	fmt.Println("Temp dir name:", dname)
 
-	o3, err := f.Seek(6, io.SeekStart)
-	check(err)
-	b3 := make([]byte, 2)
-	n3, err := io.ReadAtLeast(f, b3, 2)
-	check(err)
-	fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
+	defer os.RemoveAll(dname)
 
-	_, err = f.Seek(0, io.SeekStart)
+	fname := filepath.Join(dname, "file1")
+	err = os.WriteFile(fname, []byte{1, 2}, 0666)
 	check(err)
-
-	r4 := bufio.NewReader(f)
-	b4, err := r4.Peek(5)
-	check(err)
-	fmt.Printf("5 bytes: %s\n", string(b4))
-
-	f.Close()
 }
